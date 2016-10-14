fca6d00f8f3a1aff0c24e904511b1bf537f709c0
Damien Frikha <damien.frikha@outlook.com>
2016-10-14 08:55:09 +0200

    Exemple ListTable ok.

--- src/views/list.tsx
+++ src/views/list.tsx
@@ -15,15 +15,27 @@ export class List extends React.Component<{}, void> {
 
     render() {
         return ListTable.create(Object.assign({}, connectToListStore(listStore), {
-            columns: [],
-            LineComponent: (props: LineProps<Contact>) => (
+            columns: [{
+                label: "Nom",
+                noSort: true
+            }, {
+                label: "Prénom",
+                noSort: true
+            }, {
+                label: "Email",
+                noSort: true
+            }, {
+                label: "Civilité",
+                noSort: true
+            }],
+            LineComponent: observer((props: LineProps<Contact>) => (
                 <tr>
                     <td>{props.data!.nom}</td>
                     <td>{props.data!.prenom}</td>
                     <td>{props.data!.email}</td>
                     <td>{stringFor({$entity: ContactEntity.fields.civiliteCode, value: props.data!.civiliteCode}, {values: referenceStore.civilite, labelKey: "libelle"})}</td>
                 </tr>
-            )
+            ))
         }));
     }
 }
