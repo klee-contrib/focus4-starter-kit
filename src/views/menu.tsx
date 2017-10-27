a09c824184c2f11bce8519e6ee45037dbb6fda2b
Damien Frikha <damien.frikha@kleegroup.com>
2017-10-27 19:51:40 +0200

    8.2.1

--- src/views/menu.tsx
+++ src/views/menu.tsx
@@ -1,19 +1,19 @@
 import {observer, React} from "focus4";
-import {Menu} from "focus4/layout";
+import {Menu, MenuItem} from "focus4/layout";
 
 import {router} from "../stores";
 
 export const StarterMenu = observer(() => (
-    <Menu
-        activeRoute={router.currentStore.prefix}
-        menus={[{
-            icon: "home",
-            onClick: () => router.to("home"),
-            route: "home"
-        }, {
-            icon: "business",
-            onClick: () => router.to("test"),
-            route: "test"
-        }]}
-    />
+    <Menu activeRoute={router.currentStore.prefix}>
+        <MenuItem
+            icon="home"
+            onClick={() => router.to("home")}
+            route="home"
+        />
+        <MenuItem
+            icon="business"
+            onClick={() => router.to("test")}
+            route="test"
+        />
+    </Menu>
 ));
