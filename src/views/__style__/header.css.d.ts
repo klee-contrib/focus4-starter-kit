b252bce7fa73d2fa757427cbefceca4f57fd0f66
Damien <damien.frikha@kleegroup.com>
2023-10-21 19:23:44 +0200

    11.11.0-rc.2

--- src/views/__style__/header.css.d.ts
+++ src/views/__style__/header.css.d.ts
@@ -1,15 +1,14 @@
-import {CSSElement} from "@focus4/styling";
+import {CSSElement, CSSMod} from "@focus4/styling";
 
 interface Button { _c7660: void }
 interface Header { _c1afb: void }
-interface Summary { _2aa7d: void }
-interface User { _9d724: void }
+interface Item { _e862a: void }
 
 export interface HeaderCss {
     button: CSSElement<Button>;
     header: CSSElement<Header>;
-    summary: CSSElement<Summary>;
-    user: CSSElement<User>;
+    item: CSSElement<Item>;
+    "item--user": CSSMod<"user", Item>;
 }
 
 declare const headerCss: HeaderCss;
--- src/views/header.tsx
+++ src/views/header.tsx
@@ -1,34 +1,34 @@
 import {useObserver} from "mobx-react";
 import {ReactNode} from "react";
 
-import {HeaderBarLeft, HeaderBarRight, HeaderScrolling, HeaderSummary, HeaderTopRow} from "@focus4/layout";
-import {colorScheme} from "@focus4/styling";
+import {HeaderItem, HeaderScrolling, HeaderTopRow} from "@focus4/layout";
+import {colorScheme, toBem} from "@focus4/styling";
 import {IconButton, Switch} from "@focus4/toolbox";
 
 import css from "./__style__/header.css";
 
+const theme = toBem(css);
+
 export function Header({children}: {children?: ReactNode}) {
     return useObserver(() => (
-        <HeaderScrolling canDeploy={false} theme={{scrolling: css.header}}>
+        <HeaderScrolling canDeploy={false} theme={{scrolling: theme.header()}}>
             <HeaderTopRow>
-                <HeaderBarLeft>
+                <HeaderItem theme={{item: theme.item()}}>
                     <strong>Starter Kit Focus v4</strong>
-                </HeaderBarLeft>
-                <HeaderSummary>
-                    <div className={css.summary}>{children}</div>
-                </HeaderSummary>
-                <HeaderBarRight>
-                    <div className={css.user}>
-                        <strong>Utilisateur connecté [Administrateur]</strong>
-                        <IconButton className={css.button} icon="account_circle" />
-                        <Switch
-                            iconOff="light_mode"
-                            iconOn="dark_mode"
-                            onChange={() => (colorScheme.dark = !colorScheme.dark)}
-                            value={colorScheme.dark}
-                        />
-                    </div>
-                </HeaderBarRight>
+                </HeaderItem>
+                <HeaderItem fillWidth theme={{item: theme.item()}}>
+                    {children}
+                </HeaderItem>
+                <HeaderItem theme={{item: theme.item({user: true})}}>
+                    <strong>Utilisateur connecté [Administrateur]</strong>
+                    <IconButton className={css.button} icon="account_circle" />
+                    <Switch
+                        iconOff="light_mode"
+                        iconOn="dark_mode"
+                        onChange={() => (colorScheme.dark = !colorScheme.dark)}
+                        value={colorScheme.dark}
+                    />
+                </HeaderItem>
             </HeaderTopRow>
         </HeaderScrolling>
     ));
