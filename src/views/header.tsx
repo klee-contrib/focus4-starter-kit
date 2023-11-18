2a98195ca809424cfd9a19665b752e6115678ddb
Damien <damien.frikha@kleegroup.com>
2023-11-18 23:59:09 +0100

    11.12.0-beta.0

--- src/views/header.tsx
+++ src/views/header.tsx
@@ -1,6 +1,7 @@
 import {useObserver} from "mobx-react";
 import {ReactNode} from "react";
 
+import {messageStore} from "@focus4/core";
 import {HeaderItem, HeaderScrolling, HeaderTopRow} from "@focus4/layout";
 import {colorScheme, toBem} from "@focus4/styling";
 import {IconButton, Switch} from "@focus4/toolbox";
@@ -21,7 +22,16 @@ export function Header({children}: {children?: ReactNode}) {
                 </HeaderItem>
                 <HeaderItem theme={{item: theme.item({user: true})}}>
                     <strong>Utilisateur connecté [Administrateur]</strong>
-                    <IconButton className={css.button} icon="account_circle" />
+                    <IconButton
+                        className={css.button}
+                        icon="account_circle"
+                        onClick={() =>
+                            messageStore.addInformationMessage({
+                                label: "Il n'y a pas d'utilisateur connecté...",
+                                action: {label: "Se connecter", onClick: () => console.info("¯\\_(ツ)_/¯")}
+                            })
+                        }
+                    />
                     <Switch
                         iconOff="light_mode"
                         iconOn="dark_mode"
