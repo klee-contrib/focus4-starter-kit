187d4387826cda74832e7d8469f90ed05af6c629
Damien <damien.frikha@kleegroup.com>
2025-06-16 17:25:39 +0200

    v12.2.3

--- src/views/menu.tsx
+++ src/views/menu.tsx
@@ -10,11 +10,11 @@ import css from "./__style__/menu.css";
 export function StarterMenu() {
     const {t} = useTranslation();
     return (
-        <MainMenu activeRoute={router.get(x => x)} showOverlay theme={{list: css.list}}>
+        <MainMenu activeRoute={router.get()} showOverlay theme={{list: css.list}}>
             <div className={css.logo}>
                 <FontIcon>home_repair_service</FontIcon>
             </div>
-            <MainMenuItem href={router.href(x => x)} icon="home" label={t("router.root")} route="" />
+            <MainMenuItem href={router.href()} icon="home" label={t("router.root")} route="" />
             <MainMenuItem
                 href={router.href(x => x("utilisateurs"))}
                 icon="group"
