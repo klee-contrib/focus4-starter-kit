3468dccb40993c2f780db2171f079af9929d181c
Damien <damien.frikha@kleegroup.com>
2025-06-26 17:37:05 +0200

    v12.3

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import react from "@vitejs/plugin-react";
 import {defineConfig} from "vite";
 import checker from "vite-plugin-checker";
 import {mockDevServerPlugin} from "vite-plugin-mock-dev-server";
+import oxlintPlugin from "vite-plugin-oxlint";
 
 import {baseConfig, cssAutoModules} from "@focus4/tooling";
 
 export default defineConfig({
     ...baseConfig,
     base: "",
-    plugins: [cssAutoModules(/__style__/), react(), checker({typescript: true}), mockDevServerPlugin()],
+    plugins: [cssAutoModules(/__style__/), react(), checker({typescript: true}), oxlintPlugin(), mockDevServerPlugin()],
     server: {
         host: "0.0.0.0",
         proxy: {
